0842b1827d8606d6965b07d1dffd36b795c91a10
KrishnaDass95 <krishdass95@gmail.com>
2023-01-28 11:21:53 +0530

    JS DOM selection

--- JavaScript/JS-Basics/dom.js
+++ JavaScript/JS-Basics/dom.js
@@ -8,4 +8,17 @@ console.log(title2);
 
 // querySelector() -> gets all elements with specified selector
 let listItem = document.querySelector("li");
+listItem.innerText = "sup bro!!"
 console.log(listItem);
+
+let listItem2 = document.querySelector(".item-2");
+listItem2.innerText = "Hi my man!";
+
+//querySelectorAll()
+
+let li = document.querySelectorAll("li");
+for(let i = 0; i < li.length; i++)
+{
+    li(i).innerText = "hey";
+}
+console.log(li);
